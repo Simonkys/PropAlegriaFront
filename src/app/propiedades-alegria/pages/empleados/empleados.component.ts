058984d3c70b76a13581cfa779e70d80cb663a04
Simonky Salinas <simonasb19@gmail.com>
2023-04-14 23:59:29 -0400

    Empleados Basic Pages

--- src/app/propiedades-alegria/pages/empleados/empleados.component.ts
+++ src/app/propiedades-alegria/pages/empleados/empleados.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 
 @Component({
-  selector: 'app-empleados',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './empleados.component.html',
-  styleUrls: ['./empleados.component.scss']
+    selector: 'app-empleados',
+    standalone: true,
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        InputTextModule,
+        ToolbarModule,
+        ButtonModule,
+        TableModule,
+    ],
+    templateUrl: './empleados.component.html',
+    styleUrls: ['./empleados.component.scss'],
 })
-export class EmpleadosComponent {
-
-}
+export class EmpleadosComponent {}
