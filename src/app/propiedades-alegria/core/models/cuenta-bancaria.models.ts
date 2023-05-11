4786ed2ce37ef03ef078d747324dbe00232a681b
Simonky <simonasb19@gmail.com>
2023-05-11 09:58:02 -0400

    Interface Cuenta Bancaria

--- src/app/propiedades-alegria/core/models/cuenta-bancaria.models.ts
+++ src/app/propiedades-alegria/core/models/cuenta-bancaria.models.ts
@@ -1,8 +1,20 @@
+interface CuentaBancariaTipoCuenta {
+    id: number;
+    nom_cuenta: string;
+}
+
+interface CuentaBancariaBanco {
+    id: number;
+    nombre_banco: string;
+}
+
 export interface CuentaBancaria {
     id?: number;
+   
     cuenta: number;
     estado_cuenta: string;
     propietario_rut?: string;
-    banco_id: number;
-    tipocuenta_id: number;
+
+    banco_id: CuentaBancariaBanco;
+    tipocuenta_id: CuentaBancariaTipoCuenta;
 }
