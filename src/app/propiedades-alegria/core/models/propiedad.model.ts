f57f8d158dc8f7839a375dcef5844d0afecae64d
Simonky <simonasb19@gmail.com>
2023-05-11 09:58:28 -0400

    Interface Propiedad

--- src/app/propiedades-alegria/core/models/propiedad.model.ts
+++ src/app/propiedades-alegria/core/models/propiedad.model.ts
@@ -1,10 +1,29 @@
+interface PropiedadComuna {
+    id: number;
+    nom_com: string;
+}
+
+interface PropiedadTipoPropiedad {
+    id:              number;
+    nombre_tipoppdd: string;
+}
+
+interface PropiedadPropietario {
+    id:           number;
+    rut_prop:     string;
+    pri_nom_prop: string;
+    seg_nom_prop: string | null;
+    pri_ape_prop: string;
+    seg_ape_prop: string | null;
+}
+
 export interface Propiedad {
     id:               number;
     direccion_ppdd:   string;
     numero_ppdd:      number;
     rol_ppdd:         string | null;
-    comuna_id:        number;
-    propietario_id:   number;
-    tipopropiedad_id: number;
+    comuna_id:        PropiedadComuna;
+    propietario_id:   PropiedadPropietario;
+    tipopropiedad_id: PropiedadTipoPropiedad;
 }
 
