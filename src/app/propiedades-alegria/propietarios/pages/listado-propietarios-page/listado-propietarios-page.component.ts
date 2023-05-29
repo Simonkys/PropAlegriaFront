cbad32b7adf256cf9f8396e6fa62fcf41c03feb5
Simonky <simonasb19@gmail.com>
2023-05-29 14:44:23 -0400

    filtro y paginacion propietarios

--- src/app/propiedades-alegria/propietarios/pages/listado-propietarios-page/listado-propietarios-page.component.ts
+++ src/app/propiedades-alegria/propietarios/pages/listado-propietarios-page/listado-propietarios-page.component.ts
@@ -1,14 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
-import { TableModule } from 'primeng/table';
+import { Observable } from 'rxjs';
+
+import { Table, TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+
 import { PropietarioService } from '../../propietario.service';
+import { Propietario } from '../../propietario.model';
 
 @Component({
   selector: 'app-listado-propietarios-page',
   standalone: true,
-  imports: [CommonModule, RouterLink, TableModule, ButtonModule],
+  imports: [CommonModule, RouterLink, TableModule, ButtonModule, InputTextModule],
   templateUrl: './listado-propietarios-page.component.html',
   styleUrls: ['./listado-propietarios-page.component.scss']
 })
@@ -17,7 +22,13 @@ export class ListadoPropietariosPageComponent {
   propietariosService = inject(PropietarioService)
   router = inject(Router)
 
-  propietarios$ = this.propietariosService.getPropietarios();
+  propietarios$: Observable<Propietario[]> = this.propietariosService.getPropietarios();
+
+  filterFields: string[] = ['rut_prop', 'pri_nom_prop', 'pri_ape_prop']
+
+  clear(table: Table) {
+    table.clear();
+  }
 
 
 }
