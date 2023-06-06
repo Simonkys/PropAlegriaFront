fad1c45614b4cc41908e849e416a2a452ac6648c
Francisco <fr.gonzalezb@duocuc.cl>
2023-06-06 01:11:40 -0400

    Update registro-externo-page.component.ts

--- src/app/propiedades-alegria/externos/pages/registro-externo-page/registro-externo-page.component.ts
+++ src/app/propiedades-alegria/externos/pages/registro-externo-page/registro-externo-page.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
+
+import { ExternoService } from 'src/app/propiedades-alegria/externos/externo.service';
+import { ExternoFormComponent } from '../../components/externo-form/externo-form.component';
+import { ExternoForm } from '../../externo.model';
 
 @Component({
   selector: 'app-registro-externo-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ExternoFormComponent],
   templateUrl: './registro-externo-page.component.html',
   styleUrls: ['./registro-externo-page.component.scss']
 })
 export class RegistroExternoPageComponent {
+
+  externoService = inject(ExternoService);
+
+  location = inject(Location);
+
+  submit(externoForm: ExternoForm) {
+      this.externoService.crearExterno(externoForm)
+          .pipe()
+          .subscribe(() => this.location.back())
+  }
+
+  cancelar() {
+      this.location.back()
+  }
 
 }
