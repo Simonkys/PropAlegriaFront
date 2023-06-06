338098d7b64d68b5a3342192ed9956cefab5069b
Francisco <fr.gonzalezb@duocuc.cl>
2023-06-05 23:09:59 -0400

    Update externo.model y externo.service

--- src/app/propiedades-alegria/externos/externo.model.ts
+++ src/app/propiedades-alegria/externos/externo.model.ts
@@ -1,6 +1,16 @@
 export interface Externo {
+    id?: number;
     nombre: string;
-    rut: string;
+    rut: string | null;
+    contacto: number;
+    correo: string;
+    rol: string;
+}
+
+export interface ExternoForm {
+    id?: number;
+    nombre: string;
+    rut: string | null;
     contacto: number;
     correo: string;
     rol: string;
