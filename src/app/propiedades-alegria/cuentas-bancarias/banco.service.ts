286db0b12a6295839a65380f9a0e5f7528a653af
Simonky <simonasb19@gmail.com>
2023-06-07 13:27:09 -0400

    update: datos banco

--- src/app/propiedades-alegria/cuentas-bancarias/banco.service.ts
+++ src/app/propiedades-alegria/cuentas-bancarias/banco.service.ts
@@ -2,7 +2,7 @@ import { Injectable, inject } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { environment } from "src/environments/environment";
 import { Banco, TipoCuenta } from "./banco.model";
-import { distinctUntilChanged, shareReplay } from "rxjs";
+import { distinctUntilChanged, of, shareReplay, tap } from "rxjs";
 
 @Injectable(
     { providedIn: 'root'}
@@ -12,11 +12,33 @@ export class BancoService {
 
     private apiUrl = `${environment.apiUrl}/api`
 
+    private bancos: Banco[] = [];
+    private tipoCuentas: TipoCuenta[] = [];
+
+    private bancosLoaded = false;
+    private tipoCuentasLoaded = false;
+
     getBancos() {
-        return this.http.get<Banco[]>(`${this.apiUrl}/bancos/`)
+        if(this.bancosLoaded) {
+            return of(this.bancos);
+        }
+        return this.http.get<Banco[]>(`${this.apiUrl}/bancos/`).pipe(
+            tap((bancos) => {
+                this.bancos = bancos
+                this.bancosLoaded = true
+            })
+        )
     }
 
     getTipoCuentasBanco() {
-        return this.http.get<TipoCuenta[]>(`${this.apiUrl}/tipo_cuentas_bancos/`)
+        if(this.tipoCuentasLoaded) {
+            return of(this.tipoCuentas)
+        }
+        return this.http.get<TipoCuenta[]>(`${this.apiUrl}/tipo_cuentas_bancos/`).pipe(
+            tap((tipoCuentas) => {
+                this.tipoCuentas = tipoCuentas
+                this.tipoCuentasLoaded = true
+            })
+        )
     }
 }
