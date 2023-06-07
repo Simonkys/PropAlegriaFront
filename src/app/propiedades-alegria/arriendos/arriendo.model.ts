86e0923c29110201a68296ae87e785aaff533e7a
Simonky <simonasb19@gmail.com>
2023-06-06 20:42:37 -0400

    add: arriendo interfaces

--- src/app/propiedades-alegria/arriendos/arriendo.model.ts
+++ src/app/propiedades-alegria/arriendos/arriendo.model.ts
@@ -1,40 +1,3 @@
-interface ArriendoArrendatario {
-    id: number;
-    rut_arr: string;
-    pri_nom_arr: string;
-    pri_ape_arr: string;
-}
-
-interface ArriendoPropiedad {
-    id: number;
-    direccion_ppdd: string;
-    numero_ppdd: string | null;
-}
-
-export interface Arriendo{
-    id: number;
-
-    propiedad: ArriendoPropiedad | null;
-    arrendatario: ArriendoArrendatario;
-
-    fecha_inicio: Date;
-    fecha_termino: Date;
-
-    dia_pago: number;
-    comision: number | null;
-
-    periodo_reajuste: number;
-    fecha_reajuste: Date;
-
-    valor_arriendo: number;
-
-    fecha_entrega: Date | null;
-    estado_arriendo: boolean;
-
-    observaciones: string | null;
-
-}
-
 export interface ArriendoForm{
     id?: number;
 
@@ -64,3 +27,101 @@ export interface TablaArriendo {
     fecha_pago: Date;
     valor_arriendo: number;
 }
+
+
+export interface Arriendo{
+    id: number;
+
+    propiedad: Propiedad | null;
+    arrendatario: Arrendatario;
+
+    fecha_inicio: Date;
+    fecha_termino: Date;
+
+    dia_pago: number;
+    comision: number | null;
+
+    periodo_reajuste: number;
+    fecha_reajuste: Date;
+
+    valor_arriendo: number;
+
+    fecha_entrega: Date | null;
+    estado_arriendo: boolean;
+
+    observaciones: string | null;
+    detalle_arriendos: DetalleArriendo[]
+
+}
+
+
+export interface Arrendatario {
+    id:           number;
+    rut_arr:      string;
+    pri_nom_arr:  string;
+    seg_nom_arr:  string | null;
+    pri_ape_arr:  string;
+    seg_ape_arr:  string | null;
+    contacto_arr: number;
+    correo_arr:   string;
+    estado:       boolean;
+    saldo:        number;
+}
+
+export interface Propiedad {
+    id:                  number;
+    direccion_ppdd:      string | null;
+    numero_ppdd:         string | null;
+    rol_ppdd:            null | null;
+    cod:                 number | null;
+    nro_bodega:          null | null;
+    nro_estacionamiento: null | null;
+    valor_arriendo_base: number;
+    es_valor_uf:         boolean;
+    gas:                 string | null;
+    agua:                string | null;
+    luz:                 string | null;
+    incluye_gc:          boolean;
+    valor_gasto_comun:   number;
+    comuna:              Comuna;
+    propietario:         Propietario;
+    tipopropiedad:       Tipopropiedad;
+    externo:             number | null;
+}
+
+export interface Comuna {
+    id:      number;
+    nom_com: string;
+    reg_id:  number;
+}
+
+export interface Propietario {
+    id:                    number;
+    rut_prop:              string;
+    pri_nom_prop:          string;
+    seg_nom_prop:          string | null;
+    pri_ape_prop:          string;
+    seg_ape_prop:          string | null;
+    direccion_prop:        string;
+    email_prop:            string;
+    contacto_prop:         number;
+    pctje_cobro_honorario: number;
+    comuna:                number;
+    personalidad_juridica: number | null;
+}
+
+export interface Tipopropiedad {
+    id:                   number;
+    nombre_tipoppdd:      string;
+    descripcion_tipoppdd: string;
+}
+
+export interface DetalleArriendo {
+    id: number;
+    arriendo: number;
+    fecha_a_pagar: Date;
+    monto_a_pagar: number | null;
+    fecha_pagada: Date | null;
+    monto_pagado: number | null;
+    valor_multa: number;
+}   
