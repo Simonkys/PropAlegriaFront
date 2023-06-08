a88865e8802e1e996eebade808ce14a37c7d85da
Francisco <fr.gonzalezb@duocuc.cl>
2023-06-07 21:19:26 -0400

    Generate detalle-valor-global

---
+++ src/app/propiedades-alegria/valores-globales/components/detalle-valor-global/detalle-valor-global.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-detalle-valor-global',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './detalle-valor-global.component.html',
+  styleUrls: ['./detalle-valor-global.component.scss']
+})
+export class DetalleValorGlobalComponent {
+
+}
