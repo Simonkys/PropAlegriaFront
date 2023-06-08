2b77cfe21be352e0c40678ff0db1a5ebd8c9cb4b
Simonky <simonasb19@gmail.com>
2023-06-07 22:37:25 -0400

    up

---
+++ src/app/propiedades-alegria/core/models/dashboard.model.ts
@@ -0,0 +1,11 @@
+export interface ArriendoMultaDashboard {
+    propiedad_cod: number;
+    arrendatario: string;
+    cuenta: string;
+    fecha_pago: Date;
+    valor_arriendo: number;
+    valor_diario: number;
+    porcentaje: number;
+    multa: number;
+    dias_multa: number;
+}
