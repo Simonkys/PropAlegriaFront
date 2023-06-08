a780bb2024dea496e381d612e31df5804d9c0bc8
Simonky <simonasb19@gmail.com>
2023-06-08 14:28:54 -0400

    update: detalle propiedad navegacion a arriendo

--- src/app/propiedades-alegria/core/services/tipo-propiedades.service.ts
+++ src/app/propiedades-alegria/core/services/tipo-propiedades.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
 import { environment } from "src/environments/environment";
 import { TipoPropiedad } from "../models/tipo-propiedad.model";
+import { BehaviorSubject, tap } from "rxjs";
+
 
 @Injectable(
     {
@@ -12,8 +14,19 @@ export class TipoPropiedadesService {
     private http = inject(HttpClient);
     private apiUrl = `${environment.apiUrl}/api/tipo_propiedad/`
 
+    private tipoPropiedades = new BehaviorSubject<TipoPropiedad[]>([])
+    private loaded = false;
+
     getTipoPropiedades() {
-        return this.http.get<TipoPropiedad[]>(this.apiUrl);
+        if(this.loaded) {
+            return this.tipoPropiedades.asObservable();
+        }
+        return this.http.get<TipoPropiedad[]>(this.apiUrl).pipe(
+            tap((data) => {
+                this.tipoPropiedades.next(data)
+                this.loaded = true;
+            })
+        )
     }
 
     getTipoPropiedad(id:number){
