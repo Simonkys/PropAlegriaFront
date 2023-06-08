a9f6f647beb3a17fdb092b5d02a0bf3b34ac5aa7
Francisco <fr.gonzalezb@duocuc.cl>
2023-06-07 21:24:33 -0400

    Update detalle-valor-global.component.ts

--- src/app/propiedades-alegria/valores-globales/components/detalle-valor-global/detalle-valor-global.component.ts
+++ src/app/propiedades-alegria/valores-globales/components/detalle-valor-global/detalle-valor-global.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { ButtonModule } from 'primeng/button';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ConfirmationService } from 'primeng/api';
+
+import { ValoresGlobales } from '../../valores-globales.model';
 
 @Component({
   selector: 'app-detalle-valor-global',
   standalone: true,
-  imports: [CommonModule],
+  providers: [ConfirmationService],
+  imports: [CommonModule, ButtonModule, ConfirmPopupModule],
   templateUrl: './detalle-valor-global.component.html',
   styleUrls: ['./detalle-valor-global.component.scss']
 })
 export class DetalleValorGlobalComponent {
+
+  @Input() valoresGlobales?: ValoresGlobales
+
+  @Output() eliminarEvent = new EventEmitter()
+  @Output() actualizarEvent = new EventEmitter()
+
+  confimService = inject(ConfirmationService);
+
+  eliminar(event: Event){
+    this.confimService.confirm({
+      target: event.target || new EventTarget(),
+      message: `¿Estas segur@ de eliminar este Valor Global?`,
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.eliminarEvent.emit()
+      },
+    });
+  }
+
+  actualizar() {
+    this.actualizarEvent.emit()
+  }
 
 }
