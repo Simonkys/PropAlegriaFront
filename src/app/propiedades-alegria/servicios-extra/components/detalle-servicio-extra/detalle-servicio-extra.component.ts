eb343b004bfb654d3171a3ea3a8631f945421026
Francisco <fr.gonzalezb@duocuc.cl>
2023-06-09 00:17:15 -0400

    detalle-servicio-extra generated!

---
+++ src/app/propiedades-alegria/servicios-extra/components/detalle-servicio-extra/detalle-servicio-extra.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-detalle-servicio-extra',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './detalle-servicio-extra.component.html',
+  styleUrls: ['./detalle-servicio-extra.component.scss']
+})
+export class DetalleServicioExtraComponent {
+
+}
